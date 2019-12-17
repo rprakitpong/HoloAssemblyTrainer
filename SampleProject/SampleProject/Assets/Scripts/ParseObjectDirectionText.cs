975b490abf3143c11c865e181231b465d1fe7cd4
rprakitpong <rprakitpong@gmail.com>
2019-12-16 18:56:06 -0800

    android version working on computer

---
+++ SampleProject/SampleProject/Assets/Scripts/ParseObjectDirectionText.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ParseObjectDirectionText : MonoBehaviour
+{
+    List<string> goName;
+    List<string> direction;
+    List<string> text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Parse();
+    }
+
+    public List<string> getGONames()
+    {
+        return goName;
+    }
+
+    public List<string> getGODirections()
+    {
+        return direction;
+    }
+
+    public List<string> getGOTexts()
+    {
+        return text;
+    }
+
+    private void Parse()
+    {
+        goName = new List<string>();
+        direction = new List<string>();
+        text = new List<string>();
+        TextAsset RawTextAsset = Resources.Load<TextAsset>("ObjectDirectionTextList");
+        string RawText = RawTextAsset.text;
+        string[] EachGameObjects = RawText.Split('\n');
+        foreach (string gameobject in EachGameObjects)
+        {
+            string[] GOAttributes = gameobject.Split(',');
+            goName.Add(stringTrimmer(GOAttributes[0]));
+            direction.Add(stringTrimmer(GOAttributes[1]));
+            string text_temp = "";
+            for (int i = 2; i < GOAttributes.Length; i++)
+            {
+                text_temp = text_temp + GOAttributes[i];
+            }
+            text.Add(text_temp);
+        }
+    }
+
+    public string stringTrimmer(string str)
+    {
+        str = new string(str.Where(c => !char.IsControl(c)).ToArray());
+        return str;
+    }
+
+    public List<string> LOSTrimmer(List<string> los)
+    {
+        List<string> trimmed_los = new List<string>();
+        foreach (string str in los)
+        {
+            string trimmed_str = stringTrimmer(str);
+            trimmed_los.Add(trimmed_str);
+        }
+        return trimmed_los;
+    }
+}
--- SampleProject/SampleProject/Assets/Scripts/animateSteps.cs
+++ SampleProject/SampleProject/Assets/Scripts/animateSteps.cs
@@ -8,42 +8,82 @@ public class animateSteps : MonoBehaviour
 {
     //assume all steps has same parent
 
+    List<string> stepsName;
     List<GameObject> steps;
     List<string> description;
     List<string> orientation;
 
-    GameObject descriptionBox;
+    Text descriptionBox;
 
     List<Vector3> assembledPosition;
     List<Vector3> unassembledPosition;
 
     float timeToAnimate = 3f;
-    float offset = 1f;
+    float offset = 50f;
     int currentstep = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        steps = this.GetComponent<getObjectsAndDescriptions>().GetGameObjects();
-        description = this.GetComponent<getObjectsAndDescriptions>().GetDescriptions();
-        orientation = this.GetComponent<getObjectsAndDescriptions>().GetOrientations();
+        stepsName = this.GetComponent<ParseObjectDirectionText>().getGONames();
+        steps = new List<GameObject>();
+        description = this.GetComponent<ParseObjectDirectionText>().getGOTexts();
+        orientation = this.GetComponent<ParseObjectDirectionText>().getGODirections();
 
-        descriptionBox = GameObject.Find("descriptionBox");
+        descriptionBox = this.transform.Find("StepInfo").Find("Text").GetComponent<Text>();
 
+        initializeGOInsideSteps();
         initializePositions();
-        initializeRenderer();
+        initializeAppState();
 
-        nextStep();
-        GameObject.Find("nextButton").GetComponent<Button>().onClick.AddListener(nextStep);
-
-        Debug.Log("initialized animation");
+        this.transform.Find("StepButton").gameObject.GetComponent<Button>().onClick.AddListener(nextStep);
     }
 
-    private void initializeRenderer()
+    private void initializeGOInsideSteps()
     {
-        foreach (GameObject GO in steps)
+        List<string> stepsNameWithoutNull = new List<string>();
+        for (int i = 0; i < stepsName.Count; i++)
         {
-            GO.GetComponent<Renderer>().enabled = false;
+            GameObject go = GameObject.Find(stepsName[i]);
+            if (go != null)
+            {
+                steps.Add(go);
+                stepsNameWithoutNull.Add(stepsName[i]);
+            } else
+            {
+                description.RemoveAt(i);
+                orientation.RemoveAt(i);
+            }
+        }
+        stepsName = stepsNameWithoutNull;
+    }
+
+    private void initializeAppState()
+    {
+        ResetViewState();
+        ResetDescriptionState();
+    }
+
+    void ResetViewState()
+    {
+        for (int i = 0; i < steps.Count; i++)
+        {
+            if (orientation[i] != "init")
+            {
+                steps[i].SetActive(false);
+            }
+        }
+    }
+
+    private void ResetDescriptionState()
+    {
+        for (int i = 0; i < description.Count; i++)
+        {
+            if (orientation[i] == "init")
+            {
+                descriptionBox.text = description[i];
+                break;
+            }
         }
     }
 
@@ -55,84 +95,65 @@ public class animateSteps : MonoBehaviour
         for (int i = 0; i < steps.Count; i++)
         {
             assembledPosition.Add(steps[i].transform.localPosition);
-            
+            Vector3 v = steps[i].transform.localPosition;
             switch (orientation[i])
             {
                 case "init":
-                    unassembledPosition.Add(steps[i].transform.localPosition);
                     break;
-                case "+x":
-                    {
-                        Vector3 v = steps[i].transform.localPosition;
-                        v.x = v.x + offset * steps[i].GetComponent<Collider>().bounds.size.x;
-                        unassembledPosition.Add(v);
-                    }
+                case "+x": v.x = v.x + offset;
                     break;
-                case "-x":
-                    {
-                        Vector3 v = steps[i].transform.localPosition;
-                        v.x = v.x - offset * steps[i].GetComponent<Collider>().bounds.size.x;
-                        unassembledPosition.Add(v);
-                    }
+                case "-x": v.x = v.x - offset;
                     break;
-                case "+y":
-                    {
-                        Vector3 v = steps[i].transform.localPosition;
-                        v.y = v.y + offset * steps[i].GetComponent<Collider>().bounds.size.y;
-                        unassembledPosition.Add(v);
-                    }
+                case "+y": v.y = v.y + offset;
                     break;
-                case "-y":
-                    {
-                        Vector3 v = steps[i].transform.localPosition;
-                        v.y = v.y - offset * steps[i].GetComponent<Collider>().bounds.size.y;
-                        unassembledPosition.Add(v);
-                    }
+                case "-y": v.y = v.y - offset;
                     break;
-                case "+z":
-                    {
-                        Vector3 v = steps[i].transform.localPosition;
-                        v.z = v.z + offset * steps[i].GetComponent<Collider>().bounds.size.z;
-                        unassembledPosition.Add(v);
-                    }
+                case "+z": v.z = v.z + offset;
                     break;
-                case "-z":
-                    {
-                        Vector3 v = steps[i].transform.localPosition;
-                        v.z = v.z - offset * steps[i].GetComponent<Collider>().bounds.size.z;
-                        unassembledPosition.Add(v);
-                    }
+                case "-z": v.z = v.z - offset;
                     break;
             }
-
+            unassembledPosition.Add(v);
             steps[i].transform.localPosition = unassembledPosition[i];
         }
     }
 
     private void nextStep()
     {
-        steps[currentstep].GetComponent<Renderer>().enabled = true;
-        descriptionBox.GetComponent<Text>().text = description[currentstep];
-        StartCoroutine("MoveToPosition");
+        currentstep++;
 
-        if (currentstep + 1 >= steps.Count)
+        if (currentstep >= steps.Count)
         {
+            StopCoroutine("MoveToPosition");
             currentstep = 0;
-        } else
-        {
-            currentstep++;
+            ResetViewState();
+            ResetDescriptionState();
         }
+        else
+        {
+            steps[currentstep].SetActive(true);
+            descriptionBox.text = description[currentstep];
+            if (currentstep != 0)
+            {
+                StopCoroutine("MoveToPosition");
+                steps[currentstep - 1].transform.localPosition = assembledPosition[currentstep - 1];
+            }
+            StartCoroutine("MoveToPosition");
+        }
+
+
     }
 
     IEnumerator MoveToPosition()
     {
+        Debug.Log(currentstep);
         float time = timeToAnimate;
         float elapsedTime = 0;
         Vector3 newPosition = assembledPosition[currentstep];
         Vector3 startingPos = unassembledPosition[currentstep];
         while (elapsedTime < time)
         {
-            transform.localPosition = Vector3.Lerp(startingPos, newPosition, (elapsedTime / time));
+            steps[currentstep].transform.localPosition = Vector3.Lerp(startingPos, newPosition, (elapsedTime / time));
             elapsedTime += Time.deltaTime;
             yield return null;
         }
